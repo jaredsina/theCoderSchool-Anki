e74ce5f5febbfaf26241b1377a3ef9c0c862aa52
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-04-04 13:54:49 -0400

    Initial Commit

---
+++ theCoderSchool-Anki/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  theCoderSchool-Anki
+//
+//  Created by Jared Sinai Hernandez Adame on 4/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ theCoderSchool-Anki/theCoderSchool_AnkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  theCoderSchool_AnkiApp.swift
+//  theCoderSchool-Anki
+//
+//  Created by Jared Sinai Hernandez Adame on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct theCoderSchool_AnkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
