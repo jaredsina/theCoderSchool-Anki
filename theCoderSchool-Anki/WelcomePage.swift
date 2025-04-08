c5619400a642c9d0a9dc93d2e540efcc8b2b4a77
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-04-08 12:36:03 -0400

    Add app image with title and description

--- theCoderSchool-Anki/WelcomePage.swift
+++ theCoderSchool-Anki/WelcomePage.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct WelcomePage: View {
     var body: some View {
         VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 20) /// corner radius 0 = 90 deg bigger means more
+                    .frame(width:150,height:150)
+                    .foregroundStyle(.white) /// every apps accent color defaults to blue
+                Image(systemName:"graduationcap.fill")
+                    .font(.system(size: 70))
+                    .foregroundStyle(.blue)
+            }
+            
+                
             Text("TheCoderSchool Anki")
                 .font(.title)
                 .fontWeight(.semibold)
-            Text("Code Memorization for kids")
+                .padding(.top) /// .top really means Edge.Set.top but because of type inference we can just use .top
+                .padding(.bottom)
+            Text("Smart Code Memorization for Kids")
                 .font(.title2)
         }
+        ///.border(.orange,width:1.5) /// Borders are greating for testing layout issues if things arent behaving as you expect them to (similiar to google chrome debug tools)
+        .padding()
+        
        
     }
 }
