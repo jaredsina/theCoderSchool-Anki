a4461de9738b9460de8c2b1b8761a6393f02a5c7
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-04-18 12:00:07 -0400

    Create temp register view

--- theCoderSchool-Anki/Views/RegisterView.swift
+++ theCoderSchool-Anki/Views/RegisterView.swift
@@ -8,8 +8,51 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var errorMessage: String = ""
+    @State var name: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HeaderView(title:"Register", description:"Create an account")
+            
+            // Login Form
+            Form{
+                Section(header:Text("Register a new user").font(.title2).frame(maxWidth: .infinity, alignment:.center)){
+                    
+                    TextField("Your Name", text: $name)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .autocorrectionDisabled()
+                    
+                    /// Text Input similiar to html input
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                        .autocorrectionDisabled()
+                    
+                    
+                    /// Password Input similiar to html password input
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(DefaultTextFieldStyle())
+                }
+                
+                Section(header:Text(errorMessage).foregroundStyle(Color.red)){}
+                
+                Section(header:Button {
+                    // Attempt Register
+                 
+                } label: {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10).foregroundColor(.green)
+                        
+                        Text("Register")
+                            .foregroundColor(.white)
+                            .bold()
+                            .padding()
+                    }
+                }){}
+                Section(header:NavigationLink("Temporary Create Account Link", destination: RegisterView()).foregroundStyle(.blue)){}
+            }
+        }
     }
 }
 
