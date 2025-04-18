25992c685d39ca1e3a552efdeb6a556bb632c96e
Jared Sinai Hernandez <48105264+jaredsina@users.noreply.github.com>
2025-04-18 11:59:50 -0400

    Create link to register page

--- theCoderSchool-Anki/Views/LoginView.swift
+++ theCoderSchool-Anki/Views/LoginView.swift
@@ -13,42 +13,44 @@ struct LoginView: View {
     
     
     var body: some View {
-        VStack(spacing:0){
-            // Header
-            HeaderView()
-            
-            // Login Form
-            Form{
+        NavigationView{
+            VStack(spacing:0){
+                // Header
+                HeaderView(title: "theCoderSchool Anki", description: "Science based code repitition for kids")
                 
-                
-                
-                Section(header:Text("Login").font(.title2).frame(maxWidth: .infinity, alignment:.center)){
-                    /// Text Input similiar to html input
-                    TextField("Email Address", text: $viewModel.email)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                        .autocorrectionDisabled()
+                // Login Form
+                Form{
+                    
+                    
+                    
+                    Section(header:Text("Login").font(.title2).frame(maxWidth: .infinity, alignment:.center)){
+                        /// Text Input similiar to html input
+                        TextField("Email Address", text: $viewModel.email)
+                            .textFieldStyle(DefaultTextFieldStyle())
+                            .autocorrectionDisabled()
                         
-                     
-                    /// Password Input similiar to html password input
-                    SecureField("Password", text: $viewModel.password)
-                        .textFieldStyle(DefaultTextFieldStyle())
-                }
-               
-                Section(header:Text(viewModel.errorMessage).foregroundStyle(Color.red)){}
-                
-                Section(header:Button {
-                    // Attempt Log in
-                    viewModel.login()
-                } label: {
-                    ZStack{
-                        RoundedRectangle(cornerRadius: 10).foregroundColor(.green)
                         
-                        Text("Log In")
-                            .foregroundColor(.white)
-                            .bold()
-                            .padding()
+                        /// Password Input similiar to html password input
+                        SecureField("Password", text: $viewModel.password)
+                            .textFieldStyle(DefaultTextFieldStyle())
                     }
-                }){
+                    
+                    Section(header:Text(viewModel.errorMessage).foregroundStyle(Color.red)){}
+                    
+                    Section(header:Button {
+                        // Attempt Log in
+                        viewModel.login()
+                    } label: {
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10).foregroundColor(.green)
+                            
+                            Text("Log In")
+                                .foregroundColor(.white)
+                                .bold()
+                                .padding()
+                        }
+                    }){}
+                    Section(header:NavigationLink("Temporary Create Account Link", destination: RegisterView()).foregroundStyle(.blue)){}
                 }
             }
         }
@@ -60,14 +62,3 @@ struct LoginView: View {
 //    LoginView()
 //}
 
-struct HeaderView: View {
-    var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 30).foregroundStyle(.green)
-            VStack{
-                Text("theCoderSchool Anki").font(.title).foregroundColor(.white).bold()
-                Text("Science based code repitition for kids").font(.title3).foregroundColor(.white)
-            }
-        }.frame(width: UIScreen.main.bounds.width * 3,height: 200)
-    }
-}
